789d91beef7821c4005af06d9aeac3b6669a8544
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-08-30 21:20:54 +0530

    Created Controller and test the get patients API

---
+++ patient-service/src/main/java/org/pm/patientservice/controller/PatientController.java
@@ -0,0 +1,29 @@
+package org.pm.patientservice.controller;
+
+import org.pm.patientservice.dto.PatientResponseDTO;
+import org.pm.patientservice.service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+@CrossOrigin
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients(){
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+}
